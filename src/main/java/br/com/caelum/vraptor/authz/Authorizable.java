de475b106c9694bb18544d001e022f82c03a2de9
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2010-12-15 15:51:02 -0200

    javadoc

--- src/main/java/br/com/caelum/vraptor/authz/Authorizable.java
+++ src/main/java/br/com/caelum/vraptor/authz/Authorizable.java
@@ -2,8 +2,19 @@ package br.com.caelum.vraptor.authz;
 
 import java.util.Set;
 
+/**
+ * Marks agents as authorizables.
+ * 
+ * @author douglas campos
+ */
 public interface Authorizable {
-	
+
+	/**
+	 * Returns the set of roles that this authorizable agent has. It should
+	 * never return null, but an empty set instead.
+	 * 
+	 * @return the set of roles
+	 */
 	public Set<Role> roles();
 
 }
